552271b10ea8a6d94811aa5ece7393523bd02b48
MissDine <enidnyatichi1212@gmail.com>
2022-09-07 12:50:28 +0300

    Feature edit

---
+++ Array.js
@@ -0,0 +1,9 @@
+// Array-A list of items 
+// Array mathods
+
+// .forEach() 
+const numbers = [1,2,3,4,5,6]
+console.log(numbers);
+numbers.forEach((number, index)=>{
+    console.log(index, number);
+})
