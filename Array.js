115422b7d197e3a4f905474aa3d53443c97a74a2
MissDine <enidnyatichi1212@gmail.com>
2022-09-07 12:57:33 +0300

    Feature edit

--- Array.js
+++ Array.js
@@ -4,6 +4,14 @@
 // .forEach() 
 const numbers = [1,2,3,4,5,6]
 console.log(numbers);
-numbers.forEach((number, index)=>{
-    console.log(index, number);
+numbers.forEach((number, index, arr)=>{
+    console.log(index, number,arr);
 })
+// sum of numbers 
+let sum = 0
+numbers.forEach((number)=>{
+    sum += number
+})
+console.log(sum);
+// Number of times a letter appears in an array
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "a"]
